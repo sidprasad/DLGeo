7bee72d89d0b100dad3fdaabe5ab12ce8884a1b0
Siddhartha Prasad <siddharthaprasad@siddharthas-mbp.devices.brown.edu>
2022-05-04 16:42:54 -0400

    Ahhhhh, doesn't work right

--- code/main.py
+++ code/main.py
@@ -1,6 +1,9 @@
 from preprocess import DataGenerator
 from unetmodel import UNetModel
 import os
+import tensorflow as tf
+import numpy as np
+from matplotlib import pyplot as plt
 
 
 def main():
@@ -25,9 +28,23 @@ def main():
     model.fit_generator(generator=training_generator,
                     use_multiprocessing=True,
                     workers=6)
+
+    # I think the issue is that the output of the model is in 3 channels, 
+    # instead of 1. Not sure why -- but a potential problem as flagged below.
     
     print('Saving model!')
-    model.save(dirname)
+    model.save(os.path.join(dirname, 'model'))
+
+
+    image = training_generator.image_to_array(train_path + '/images/austin1.tif')
+
+    # TEST HERE
+    #image = tf.keras.preprocessing.image.load_img(train_path + '/images/austin1.tif', target_size = (256, 256))
+    input_arr = np.array([image])  # Convert single image to a batch.
+    predictions = model.predict(input_arr)
+
+    plt.imshow((predictions[0] * 255).astype(np.uint8))
+    plt.show()
 
 
 if __name__ == '__main__':
